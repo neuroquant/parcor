165bbc1b8aee5f1b8d7622562554a2d52ee29f16
Manjari Narayan (VA) <manjari.narayan@gmail.com>
2016-07-07 15:29:53 -0700

    Updating main.m using latest analyses

--- main.m
+++ main.m
@@ -3,10 +3,24 @@ create_opts;
 disp('Collecting datasets')
 create_dataset;
 % Add whitening
+disp('Remove dependencies')
+run_kggm_whitening
 % Add resampling
 disp('Estimate partial correlation networks')
 run_parcor;
 disp('Compute network metrics for all partial correlation networks')
 compute_network_metrics;
+
+% Export data
+% run_export_allconditions
+
+% Plot (exploratory)
+% grammplot_metrics
+
+% Conduct inference (TBD)
+
+% Correct for multiple testing (TBD)
+
+% grammplot_metrics
 
 % exit;
