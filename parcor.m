98ebf32b4ab21302d5bc9c7324039ce7911bf476
Manjari Narayan <manjari.narayan@gmail.com>
2016-10-30 22:51:42 -0700

    Removed centering bug. Uncentered  mean was messing up shrinkage calc. Added Ledoit-Wolf constant correlation target option. Returns shrunken covariance also

--- parcor.m
+++ parcor.m
@@ -29,29 +29,64 @@ function result = parcor(X,opts)
 	[m p] = size(X);
 
 	assert(length(size(X))==2,'X has wrong dimensions');
-	assert(m>p,'Sample size is inadequate for this function')
-	try
+	assert(m>10*log(p),'Sample size is inadequate for this function')
+	try			
+		meanX_i = zeros(1,p); 
 		assert(abs(sum(mean(X(:,:,1))))<1e-10,'Not zero-mean centered. Please center time-series');
-		meanX_i = zeros(1,p);
 	catch
-		meanX_i = mean(X,1);
-		X = bsxfun(@minus,X,mean(X,1));
-		assert(abs(sum(mean(X(:,:,1))))<1e-10,'Unsuccessful centering time-series')
+		disp('Centering time-series')
+		if(abs(sum(mean(X(:,:,1))))>1e-10)
+			meanX_i = mean(X,1);
+			X = bsxfun(@minus,X,mean(X,1));
+			assert(abs(sum(mean(X(:,:,1))))<1e-10,'Unsuccessful centering time-series')
+			meanX_i = mean(X,1);
+		else
+			meanX_i = zeros(1,p); 
+		end	
 	end
 
 	Sighat = cov(X);
 	[V D] = eig(Sighat);
-
-	% Rescale Sighat to be correlation matrix
-	diagW = diag(diag(Sighat));
-	Sighat = inv(sqrt(diagW))*Sighat*inv(sqrt(diagW));
-	Xcorr = bsxfun(@rdivide,X,diag(sqrt(diagW))');
 
 	try
 		assert(sum(diag(D)<=0)==0,'Zero or Negative Eigenvalues');
 	catch
 		disp('Covariance matrix is not positive definite');
 	end
+	
+	% Rescale Sighat to be correlation matrix
+	diagW = diag(diag(Sighat));
+	Sighat = inv(sqrt(diagW))*Sighat*inv(sqrt(diagW));
+	Xcorr = bsxfun(@rdivide,X,diag(sqrt(diagW))');
+
+	try
+		assert(sum(diag(diagW)<=0)==0,'Zero or Negative Variances');
+	catch
+		disp('Covariance matrix is not positive definite');
+		sum(diag(diagW)<=0)
+	end
+
+
+
+	if(isempty(opts))
+		Target = [];
+	elseif(isfield(opts,'Target'))
+		Target = opts.Target;
+	else
+		switch opts.ridgeType
+		case 0
+			Target = [];			
+		case 1
+			Target = diag(diag(Sighat)); % Diagonal matrix with variances from Sighat.
+		case 2
+			Target = eye(p);
+		case 3
+			meancorr = sum(sum(triu(Sighat,1)));
+			meancorr = meancorr/nchoosek(p,2);
+			Target  = eye(p) + ~eye(p)*meancorr;	% Ledoit Wolf with constant-correlation off diagonals 
+		end
+	end
+		
 
 	if(isempty(opts)|isempty(opts.lambda))
 		% if(min(diag(D))>.001)
@@ -71,7 +106,7 @@ function result = parcor(X,opts)
 			% Shaffer-Strimmer analytical lambda
 			% opts.lambda_ss = sum(sig_var) - cov(target,sigma) - bias(sigma)(target-sigma)/sum((target-sigma).^2);
 			% opts.lambda = max(0,min(1,opts.lambda_ss));
-			if(opts.ridgeType==1)
+			if(opts.ridgeType==1|opts.ridgeType==3)
 
 				% Shaffer-Strimmer, analytical formula for type D
 				for ii=1:p
@@ -80,7 +115,12 @@ function result = parcor(X,opts)
 						W_kij(:,jj,ii) = W_kij(:,ii,jj);
 					end
 				end
-				assert(sum(sum(triu(Sighat-squeeze(mean(W_kij,1)),1).^2))/nchoosek(p,2)< 1e-3,'Verifying mean W_kij is empirical covariance');
+				if(m>p)
+					assert(sum(sum(triu(Sighat-squeeze(mean(W_kij,1)),1).^2))/nchoosek(p,2)< 1e-3,'Verifying mean W_kij is empirical covariance');
+				else
+					disp('Frob norm error between empirical cov and sample cov')
+					sum(sum(triu(Sighat-squeeze(mean(W_kij,1)),1).^2))/nchoosek(p,2)
+				end
 				var_Sighat = m/(m-1)^3*squeeze(sum(bsxfun(@minus,W_kij,reshape(Sighat, [1 p p])).^2,1));
 				opts.lambda_ss = sum(sum(triu(var_Sighat,1)))/sum(sum(triu(Sighat.^2,1)));
 				opts.lambda = max(0,min(1,opts.lambda_ss));
@@ -115,26 +155,30 @@ function result = parcor(X,opts)
 	switch useRidge
 
 	case 0
-		Target = [];
-		Theta = inv(Sighat);
+		%Target = [];
+		Sigma = Sighat;
+		Theta = pinv(Sigma);
 	case 1
 		disp('Using Ridge Penalization, Type I')
-		Target = diag(diag(Sighat)); % Diagonal matrix with variances from Sighat.
-
-		Theta = inv((1-opts.lambda)*Sighat + opts.lambda*Target); % Type I, Ledoit-Wolf Ridge Penalty.
+		%Target = diag(diag(Sighat)); % Diagonal matrix with variances from Sighat.
+		Sigma = (1-opts.lambda)*Sighat + opts.lambda*Target;
+		Theta = pinv(Sigma); % Type I, Ledoit-Wolf Ridge Penalty.
+		
 	case 2
 		disp('Using Ridge Penalization, Type II')
-		Target = eye(p);
-
+		%Target = eye(p);
 		% Shaffer-Strimmer, analytical formula for type A
-
-		Theta = inv(Sighat+opts.lambda*eye(p)); % Type II, Ridge Penalty
+		Sigma = Sighat+opts.lambda*eye(p);
+		Theta = pinv(Sigma); % Type II, Ridge Penalty
+	case 3
+		disp('Using Ridge Penalization, Type I w. Constant Correlation Target')
+		Sigma = (1-opts.lambda)*Sighat + opts.lambda*Target;
+		Theta = pinv(Sigma); % Type I, Ledoit-Wolf Ridge Penalty.
 	end
 
 	diagTheta = diag(diag(Theta));
 	Rho = -inv(sqrt(diagTheta))*triu(Theta,1)*inv(sqrt(diagTheta));
 	Rho = eye(p)+Rho+Rho';
-
 	% Rescaling back Theta, since Sighat is correlation matrix
 	Theta = inv(sqrt(diagW))*Theta*inv(sqrt(diagW));
 
@@ -146,6 +190,7 @@ function result = parcor(X,opts)
 	end
 
 	result.Rho = Rho;
+	result.Sigma = Sigma;
 	result.Theta = Theta;
 	result.Var = diag(diagW); % Variances
 	result.InvVar = diag(diagTheta); % Inverse Variances used to normalize partial correlation matrix.
