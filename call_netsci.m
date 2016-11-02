cdf60e00d2cea8edaf920d5c08135b11e34ffb75
Manjari Narayan <manjari.narayan@gmail.com>
2016-11-01 21:29:02 -0700

    Weighted option, current flow was not being scaled

--- call_netsci.m
+++ call_netsci.m
@@ -57,8 +57,11 @@ for metric_no = 1:length(opts.bct_num)
 					end
 				case 'current_flow_metrics'
 					tmp_stats = feval(@callnetworkx,affinitySig,1,0);
-					if(isScaled)
+					if(isScaled==1)
 						tmp_stats(:,1) = tmp_stats(:,1)/((p-1)*(p-2));
+					elseif(isScaled==2)
+						tmp_stats(:,1) = (tmp_stats(:,1)-min(tmp_stats(:,1)))./(max(tmp_stats(:,1))-min(tmp_stats(:,1))); 
+						tmp_stats(:,1) = (tmp_stats(:,2)-min(tmp_stats(:,2)))./(max(tmp_stats(:,2))-min(tmp_stats(:,2))); 						
 					end
 				case 'efficiency_wei'
 					tmp_stats = feval(bct_funs{bct_num},affinitySig,1);
